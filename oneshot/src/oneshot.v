8c7a4b248ac9a64dcfbf691c7b621f37a3f49f7c
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2019-10-02 21:56:27 +0900

    first commit. add module oneshot

---
+++ oneshot/src/oneshot.v
@@ -0,0 +1,35 @@
+module ONESHOT(
+    input clk, reset_n,
+    input in,
+    output reg enable
+);
+
+reg [1:0] cnt;
+reg state;
+
+always @(posedge in) begin
+    state <= 1;
+end
+
+always @(posedge clk) begin
+    if(!reset_n) begin
+        enable <= 1'b0;
+        state <= 0;
+        cnt <= 0;
+    end
+    else begin
+        if(cnt == 2'b11) begin
+            enable <= 0;
+            state <= 0;
+            cnt <= 0;
+        end
+        else begin
+            if(state) begin
+                enable <= 1;
+                cnt = cnt + 1;
+            end
+        end
+    end
+end
+
+endmodule
---
+++ oneshot/src/test_oneshot.v
@@ -0,0 +1,47 @@
+`timescale 1 ns/ 100 ps
+
+module TEST_ONESHOT();
+
+reg clk;
+reg reset_n;
+reg signal;
+wire enable;
+
+always begin
+    #1 clk <= ~clk;
+end
+
+initial begin
+    $dumpvars(0, TEST_ONESHOT);
+    #0
+        clk <= 0;
+        signal <= 0;
+        reset_n <= 1;
+    #0;
+    #5 reset_n <= 0;
+    #1 reset_n <= 1;
+    
+    #5 signal <= 1;
+    #1 signal <= 0;
+    
+    #10 signal <= 1;
+    #2 signal <= 0;
+    
+    #10 signal <= 1;
+    #3 signal <= 0;
+    
+    #10 signal <= 1;
+    #10 signal <= 0;
+    
+    #50 $finish;
+end
+
+
+ONESHOT o(
+    .clk(clk),
+    .reset_n(reset_n),
+    .in(signal),
+    .enable(enable)
+);
+
+endmodule
