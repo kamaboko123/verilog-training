c54032f9e2e423825cce137d5f43d7dffc0ee739
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2019-10-04 20:38:20 +0900

    update i2c master module

--- i2c_master/src/i2c_master.v
+++ i2c_master/src/i2c_master.v
@@ -3,7 +3,8 @@ module I2C_MASTER(
     input enable,
     input mode,
     input [6:0] slave_addr,
-    input [31:0] data,
+    input [7:0] data,
+    input stop,
     output scl,
     inout sda,
     output busy,
@@ -18,15 +19,23 @@ assign sda = _sda;
 assign busy = ~(_state == 3'd0);
 
 reg [2:0] _state;
+reg [2:0] _state_next;
 reg [7:0] _slave_addr;
-reg [31:0] _data;
+reg [7:0] _data;
 reg [3:0] _step;
 reg [2:0] _cnt;
+reg _stop;
 
+parameter MODE_WRITE = 1;
+parameter MODE_READ = 0;
 parameter STATE_READY = 0;
 parameter STATE_START = 1;
 parameter STATE_SEND_ADDR = 2;
 parameter STATE_CHECK_ACK = 3;
+parameter STATE_SEND_DATA = 4;
+parameter STATE_STOP = 5;
+parameter STATE_NONE = 6;
+parameter STATE_ERROR = 7;
 
 always @(negedge reset_n) begin
     _state <= STATE_READY;
@@ -39,11 +48,13 @@ always @(negedge reset_n) begin
 end
 
 always @(posedge clk) begin
-    if(enable) begin
+    if(enable && (_state == STATE_READY)) begin
         _enable <= 1;
         _mode <= mode;
         _slave_addr <= (slave_addr << 1) + mode;
+        _data = data;
         _state <= STATE_START;
+        _stop <= stop;
         error <= 0;
     end
     else if(_enable) begin
@@ -77,6 +88,12 @@ always @(posedge clk) begin
                     if(_cnt == 0) begin
                         _step <= 0;
                         _state <= STATE_CHECK_ACK;
+                        if(_mode == MODE_WRITE) begin
+                            _state_next <= STATE_SEND_DATA;
+                        end
+                        else begin
+                            //_state_next <= STATE_RECV_DATA;
+                        end
                     end
                     else begin
                         _step <= 1;
@@ -85,7 +102,7 @@ always @(posedge clk) begin
             endcase
         end
         
-        if (_state == STATE_CHECK_ACK) begin
+        if(_state == STATE_CHECK_ACK) begin
             case(_step)
                 0:begin
                     _scl <= 0;
@@ -96,13 +113,79 @@ always @(posedge clk) begin
                     _step <= 2;
                 end
                 2:begin
-                    if(sda == 0)begin
+                    _step <= 0;
+                    if(sda == 0) begin
+                        _step <= 0;
+                        _state <= _state_next;
                     end
                     else begin
-                        error <= 1;
+                        _state <= STATE_ERROR;
                     end
                 end
             endcase
+        end
+        
+        if(_state == STATE_SEND_DATA) begin
+            case (_step)
+                0:begin
+                    _cnt <= 3'd7;
+                    _step <= 1;
+                end
+                1:begin
+                    _scl <= 0;
+                    _step <= 2;
+                end
+                2:begin
+                    _sda <= (_data & 8'b10000000) ? 1'b1 : 1'b0;
+                    _step <= 3;
+                end
+                3:begin
+                    _data <= _data << 1;
+                    _scl <= 1;
+                    _step <= 4;
+                end
+                4:begin
+                    _cnt <= _cnt - 1;
+                    if(_cnt == 0) begin
+                        _step <= 0;
+                        _state <= STATE_CHECK_ACK;
+                        _state_next <= STATE_STOP;
+                    end
+                    else begin
+                        _step <= 1;
+                    end
+                end
+            endcase
+        end
+        
+        if(_state == STATE_STOP) begin
+            case(_step)
+                0:begin
+                    _scl <= 0;
+                    _step <= 1;
+                end
+                1:begin
+                    _sda <= 0;
+                    _step <= 2;
+                end
+                2:begin
+                    _scl <= 1;
+                    _step <= 3;
+                end
+                3:begin
+                    _sda <= 1;
+                    _step <= 4;
+                end
+                4:begin
+                    _state <= STATE_READY;
+                end
+            endcase
+        end
+        
+        if(_state == STATE_ERROR) begin
+            error <= 1;
+            _step <= 0;
+            _state <= STATE_STOP;
         end
         
     end
--- i2c_master/src/test_i2c_master.v
+++ i2c_master/src/test_i2c_master.v
@@ -11,6 +11,7 @@ wire enable;
 
 reg i2c_mode;
 reg [6:0] i2c_slave_addr;
+reg [7:0] i2c_data;
 
 wire sda;
 reg _sda;
@@ -28,6 +29,7 @@ initial begin
         reset_n <= 1;
         i2c_mode <= 1;
         i2c_slave_addr <= 7'h77;
+        i2c_data <= 8'hda;
         _sda <= 1'bz;
     #(CLK + 1);
     
@@ -37,7 +39,10 @@ initial begin
     #5 signal <= 1;
     #1 signal <= 0;
     
-    #810 _sda <= 0;
+    #750 _sda <= 0;
+    #30 _sda <= 1'bz;
+    #700 _sda <= 0;
+    #30 _sda <= 1'bz;
     
     #2000 $finish;
 end
@@ -56,6 +61,7 @@ I2C_MASTER master(
     .enable(enable),
     .mode(i2c_mode),
     .slave_addr(i2c_slave_addr),
+    .data(i2c_data),
     .sda(sda)
 );
 
