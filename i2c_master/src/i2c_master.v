4a71c53725ff69523253371dddbc5ee64e97f669
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2019-10-04 21:34:09 +0900

    update

--- i2c_master/src/i2c_master.v
+++ i2c_master/src/i2c_master.v
@@ -48,150 +48,151 @@ always @(posedge clk or negedge reset_n or posedge enable) begin
         _enable <= 0;
         error <= 0;
     end
-    
-    if(enable) begin
-        if(_state == STATE_READY)begin
-            _enable <= 1;
-            _mode <= mode;
-            _slave_addr <= (slave_addr << 1) + mode;
-            _data = data;
-            _state <= STATE_START;
-            _stop <= stop;
-            error <= 0;
-        end
-    end
     else begin
-        if(_enable) begin
-            if(_state == STATE_START) begin
-                _sda <= 0;
-                _step <= 0;
-                _state <= STATE_SEND_ADDR;
+        if(enable) begin
+            if(_state == STATE_READY)begin
+                _enable <= 1;
+                _mode <= mode;
+                _slave_addr <= (slave_addr << 1) + mode;
+                _data = data;
+                _state <= STATE_START;
+                _stop <= stop;
+                error <= 0;
             end
-            
-            if(_state == STATE_SEND_ADDR) begin
-                case (_step)
-                    0:begin
-                        _cnt <= 3'd7;
-                        _step <= 1;
-                    end
-                    1:begin
-                        _scl <= 0;
-                        _step <= 2;
-                    end
-                    2:begin
-                        _sda <= (_slave_addr & 8'b10000000) ? 1'b1 : 1'b0;
-                        _step <= 3;
-                    end
-                    3:begin
-                        _slave_addr <= _slave_addr << 1;
-                        _scl <= 1;
-                        _step <= 4;
-                    end
-                    4:begin
-                        _cnt <= _cnt -1;
-                        if(_cnt == 0) begin
-                            _step <= 0;
-                            _state <= STATE_CHECK_ACK;
-                            if(_mode == MODE_WRITE) begin
-                                _state_next <= STATE_SEND_DATA;
+        end
+        else begin
+            if(_enable) begin
+                if(_state == STATE_START) begin
+                    _sda <= 0;
+                    _step <= 0;
+                    _state <= STATE_SEND_ADDR;
+                end
+                
+                if(_state == STATE_SEND_ADDR) begin
+                    case (_step)
+                        0:begin
+                            _cnt <= 3'd7;
+                            _step <= 1;
+                        end
+                        1:begin
+                            _scl <= 0;
+                            _step <= 2;
+                        end
+                        2:begin
+                            _sda <= (_slave_addr & 8'b10000000) ? 1'b1 : 1'b0;
+                            _step <= 3;
+                        end
+                        3:begin
+                            _slave_addr <= _slave_addr << 1;
+                            _scl <= 1;
+                            _step <= 4;
+                        end
+                        4:begin
+                            _cnt <= _cnt -1;
+                            if(_cnt == 0) begin
+                                _step <= 0;
+                                _state <= STATE_CHECK_ACK;
+                                if(_mode == MODE_WRITE) begin
+                                    _state_next <= STATE_SEND_DATA;
+                                end
+                                else begin
+                                    //_state_next <= STATE_RECV_DATA;
+                                end
                             end
                             else begin
-                                //_state_next <= STATE_RECV_DATA;
+                                _step <= 1;
                             end
                         end
-                        else begin
+                    endcase
+                end
+                
+                if(_state == STATE_CHECK_ACK) begin
+                    case(_step)
+                        0:begin
+                            _scl <= 0;
                             _step <= 1;
                         end
-                    end
-                endcase
-            end
-            
-            if(_state == STATE_CHECK_ACK) begin
-                case(_step)
-                    0:begin
-                        _scl <= 0;
-                        _step <= 1;
-                    end
-                    1:begin
-                        _sda <= 1'bz;
-                        _step <= 2;
-                    end
-                    2:begin
-                        _step <= 0;
-                        if(sda == 0) begin
+                        1:begin
+                            _sda <= 1'bz;
+                            _step <= 2;
+                        end
+                        2:begin
                             _step <= 0;
-                            _state <= _state_next;
+                            if(sda == 0) begin
+                                _step <= 0;
+                                _state <= _state_next;
+                            end
+                            else begin
+                                _state <= STATE_ERROR;
+                            end
                         end
-                        else begin
-                            _state <= STATE_ERROR;
-                        end
-                    end
-                endcase
-            end
-            
-            if(_state == STATE_SEND_DATA) begin
-                case (_step)
-                    0:begin
-                        _cnt <= 3'd7;
-                        _step <= 1;
-                    end
-                    1:begin
-                        _scl <= 0;
-                        _step <= 2;
-                    end
-                    2:begin
-                        _sda <= (_data & 8'b10000000) ? 1'b1 : 1'b0;
-                        _step <= 3;
-                    end
-                    3:begin
-                        _data <= _data << 1;
-                        _scl <= 1;
-                        _step <= 4;
-                    end
-                    4:begin
-                        _cnt <= _cnt - 1;
-                        if(_cnt == 0) begin
-                            _step <= 0;
-                            _state <= STATE_CHECK_ACK;
-                            _state_next <= STATE_STOP;
-                        end
-                        else begin
+                    endcase
+                end
+                
+                if(_state == STATE_SEND_DATA) begin
+                    case (_step)
+                        0:begin
+                            _cnt <= 3'd7;
                             _step <= 1;
                         end
-                    end
-                endcase
+                        1:begin
+                            _scl <= 0;
+                            _step <= 2;
+                        end
+                        2:begin
+                            _sda <= (_data & 8'b10000000) ? 1'b1 : 1'b0;
+                            _step <= 3;
+                        end
+                        3:begin
+                            _data <= _data << 1;
+                            _scl <= 1;
+                            _step <= 4;
+                        end
+                        4:begin
+                            _cnt <= _cnt - 1;
+                            if(_cnt == 0) begin
+                                _step <= 0;
+                                _state <= STATE_CHECK_ACK;
+                                _state_next <= STATE_STOP;
+                            end
+                            else begin
+                                _step <= 1;
+                            end
+                        end
+                    endcase
+                end
+                
+                if(_state == STATE_STOP) begin
+                    case(_step)
+                        0:begin
+                            _scl <= 0;
+                            _step <= 1;
+                        end
+                        1:begin
+                            _sda <= 0;
+                            _step <= 2;
+                        end
+                        2:begin
+                            _scl <= 1;
+                            _step <= 3;
+                        end
+                        3:begin
+                            _sda <= 1;
+                            _step <= 4;
+                        end
+                        4:begin
+                            _state <= STATE_READY;
+                        end
+                    endcase
+                end
+                
+                if(_state == STATE_ERROR) begin
+                    error <= 1;
+                    _step <= 0;
+                    _state <= STATE_STOP;
+                end
+                
             end
-            
-            if(_state == STATE_STOP) begin
-                case(_step)
-                    0:begin
-                        _scl <= 0;
-                        _step <= 1;
-                    end
-                    1:begin
-                        _sda <= 0;
-                        _step <= 2;
-                    end
-                    2:begin
-                        _scl <= 1;
-                        _step <= 3;
-                    end
-                    3:begin
-                        _sda <= 1;
-                        _step <= 4;
-                    end
-                    4:begin
-                        _state <= STATE_READY;
-                    end
-                endcase
-            end
-            
-            if(_state == STATE_ERROR) begin
-                error <= 1;
-                _step <= 0;
-                _state <= STATE_STOP;
-            end
-            
         end
     end
 end
